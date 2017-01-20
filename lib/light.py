51504ae47a5d52c0c43368f68fd04ed81e3035e5
Ari Mandjelikian <arim215@users.noreply.github.com>
2017-01-20 16:24:25 -0500

    Update light.py

--- lib/light.py
+++ lib/light.py
@@ -16,29 +16,21 @@ def setup():
     # Setup GPIO on raspberry pi
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
-    # Tell the program you want to use pin number 7 as output
-    GPIO.setup(7, GPIO.OUT, GPIO.LOW)
-    # Set GPIO 15 as a PULL DOWN switch
-    GPIO.setup(15, GPIO.IN, GPIO.PUD_DOWN)  # If no input button connected, comment this line out
-    #GPIO.add_event_detect(15, GPIO.RISING, callback=activate_goal_light, bouncetime=5000)
+    GPIO.setup(7, GPIO.OUT, GPIO.LOW) # Tell the program you want to use pin number 7 as output
+    GPIO.setup(15, GPIO.IN, GPIO.PUD_DOWN)  # Set GPIO 15 as a PULL DOWN switch
     GPIO.add_event_detect(15, GPIO.RISING, activate_goal_light, 5000)
 
 
 def activate_goal_light(gpio_event_var=0):
-    """ Function to activate GPIO for goal light and Audio clip. """
+    """ Function to activate GPIO for goal light and plar random audio clip. """
 
-    # select random audio clip
-    # Set random numbers depending on number of audio clips available
-    songrandom = random.randint(1, 3)
-    # Set pin 7 output at high for goal light ON
-    GPIO.output(7, GPIO.LOW)
+    songrandom = random.randint(1, 3) #Set random numbers depending on number of audio clips available
+    GPIO.output(7, GPIO.LOW) #Turn on light
     # Prepare commande to play sound (change file name if needed)
     command_play_song = 'sudo mpg123 -q ./audio/goal_horn_{SongId}.mp3'.format(
         SongId=str(songrandom))
-    # Play sound
-    os.system(command_play_song)
-    # Set pin 7 output at high for goal light OFF
-    GPIO.output(7, GPIO.HIGH)
+    os.system(command_play_song) # Play sound
+    GPIO.output(7, GPIO.HIGH) #Turn off light
 
 
 def cleanup():
